d5da6591394e5a956b91c7d25c8c6bde38e55f08
RmR266 <ihaterohitmewara@gmail.com>
2025-04-13 09:47:36 +0530

    Adding Comments In JS code for explanation

--- 1-colorChanger/script.js
+++ 1-colorChanger/script.js
@@ -1,6 +1,11 @@
 const buttons = document.querySelectorAll('.button');
+// selecting all the "buttons" using querySelectorAll
 const body = document.querySelector('body');
+// selecting body using tagName
 
+// applying forEach on the nodelist we got from querySelectorAll 
+// Using EventListener to change colour using the targetid 
+ 
 buttons.forEach(function (button) {
   button.addEventListener('click', function (e) {
     if (e.target.id === 'grey') {

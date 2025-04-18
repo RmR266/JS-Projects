da82c0ae50f495c334d5763f2b1f09ad7af9d032
RmR266 <ihaterohitmewara@gmail.com>
2025-04-18 10:46:31 +0530

    adding comments for explanation

--- 6-unlimitedColors/script.js
+++ 6-unlimitedColors/script.js
@@ -1,10 +1,11 @@
 //generate a random color
 
 const randomColor = function () {
-    const hex = '0123456789ABCDEF';
-    let color = '#';
+    const hex = '0123456789ABCDEF';  // elements in a hex color code
+    let color = '#'; // hex code starts with # and then using loop to take values from hex variable and insterting it into our color variable 
+    // using i<6 as there are only 6 elements in hex code excluding #
     for (let i = 0; i < 6; i++) {
-      color += hex[Math.floor(Math.random() * 16)];
+      color += hex[Math.floor(Math.random() * 16)]; // using math.floor to get rounded value and not using +1 as we need 0 as well and using that position from genrated random values 
     }
     return color;
   };
@@ -24,8 +25,8 @@ const randomColor = function () {
     intervalId = null;
   };
   
-  document.querySelector('#start').addEventListener('click', startChangingColor);
+  document.querySelector('#start').addEventListener('click', startChangingColor); // selecting start button 
   
-  document.querySelector('#stop').addEventListener('click', stopChangingColor);
+  document.querySelector('#stop').addEventListener('click', stopChangingColor); // selection stop button 
   
   
